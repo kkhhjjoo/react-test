8d5b65e590a296b1f25281d78dcfaaa91df0d9f3
hyunjoo.kim <khjoo1203@gmail.com>
2025-07-01 15:48:32 +0900

    박스 만들기

---
+++ first-app/src/component/Box.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Box = (props) => {
+  console.log(props);
+  return (
+    <div className='box'>
+      Box1
+      <p>{props.name}</p>
+    </div>
+  );
+};
+
+export default Box;
